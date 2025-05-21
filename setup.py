0c3f4645b4e1dc1264764ec68fca81f619e6a5cc
Yasmin Avila Nunes <yasmin.nunes@amperaracing.com>
2025-05-21 17:08:28 -0300

    fix: test

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-package_name = 'test'
+package_name = 'test1'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/test1.launch.py']),
-    ],
+    ('share/ament_index/resource_index/packages', ['resource/test1']),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', ['launch/test1.launch.py']),
+],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Yasmin Avila Nunes',
+    maintainer='yasmin',
     maintainer_email='yasmin.nunes@amperaracing.com',
-    description='Meu pacote de exemplo ROS 2',
+    description='Um exemplo de nó ROS 2',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'test1_node = test1.test1_node:main',
+            'test1_node = test1.test1_node:main'
         ],
     },
 )
